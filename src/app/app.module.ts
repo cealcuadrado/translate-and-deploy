acb8061a32efa873e84ca2ac5928fa54ea52a715
CeAlCuadrado <cealcuadrado@outlook.com>
2021-10-31 07:28:08 -0300

    Definido layout básico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LayoutComponent } from './layout/layout.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent
+    LayoutComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
